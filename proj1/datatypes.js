15e52c492279a694df38fe6367d9af26611e0d20
Manthan Sharma <chavi.rawat@s.amity.edu>
2024-04-02 23:34:20 +0000

    DataType Summary

---
+++ proj1/datatypes.js
@@ -0,0 +1,31 @@
+//Premtive
+
+const score = 100
+const scoreValue = 100.3
+
+const id = Symbol('123')
+const id1 = Symbol(123)
+const id2 = Symbol('123')
+
+ console.table( id + id2);
+
+ console.log(id === id1); //false
+let BigInt = 2n
+console.log(typeof BigInt);
+// String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Reference (Non Premitive )
+
+//Array, Objects, Functions
+
+const herosArr = ["Hulk", "Antman", "Thor" ]
+
+let nameObj = {
+
+    name:"Yash",
+    age:"21"
+
+}
+
+console.log(typeof nameObj);
+console.log(typeof herosArr);
