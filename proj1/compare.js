590c7a2299907679f36ae693cccd0400205a3651
Manthan Sharma <chavi.rawat@s.amity.edu>
2024-04-02 22:44:43 +0000

    Comparisions of values

---
+++ proj1/compare.js
@@ -0,0 +1,16 @@
+console.log( "2" > 1); //true
+console.log( null < 1); //true
+console.log( null > 0); //false
+console.log( null == 0); //fasle
+console.log( null < 0); //false
+console.log( null === 0); //fasle
+console.log( null <= 0); //true
+console.log( null >= 0); //true
+console.log( NaN == 0); //fasle
+console.log( NaN > 0); //fasle
+console.log( NaN < 0); //fasle
+console.log( NaN <= 0); //fasle
+console.log( NaN >= 0); //fasle
+
+ console.log( "2" === 2); //false
+ console.log( "2" == 2); //true
