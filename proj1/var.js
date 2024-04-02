77d2e60beaf228042b22a27a54b3fa4d7071fbf5
Manthan Sharma <chavi.rawat@s.amity.edu>
2024-04-02 13:19:30 +0000

    Variables and its types

---
+++ proj1/var.js
@@ -0,0 +1,25 @@
+const accountId = 101
+let accountEmail 
+var accountPassword = "12345"
+accountCity = "Ajmer"
+let accountLoggedin = true
+let accountCr = null
+
+
+
+// accountId = 2
+
+// accountEmail = "ys@gmail.com"
+// accountPassword = "abcd"
+// accountCity = "Jaipur"
+
+// console.log(accountId);
+
+console.table( [accountId, accountEmail, accountPassword, accountCity])
+
+console.table( typeof accountId)
+console.table( typeof accountEmail)
+console.table( typeof accountPassword)
+console.table( typeof accountCity)
+console.table( typeof accountLoggedin)
+console.table( typeof accountCr)
